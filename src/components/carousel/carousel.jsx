0a04d74bf3bd2b9751b4329355cb0e6b521f0fa2
Iosif Screpciuc <iosifscrepy@gmail.com>
2023-03-11 14:02:12 +0200

    asd

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+
 import './carousel.css';
 import poza1 from '../../data/images/poza2.webp';
 import poza2 from '../../data/images/interior.webp';
@@ -9,28 +12,37 @@ import poza3 from '../../data/images/poza1.webp';
 const poze = [poza1, poza2, poza3];
 const Carousele = () => {
   return (
-    <div className="filter height">
-      <Carousel
-        showThumbs={false}
-        showStatus={false}
-        infiniteLoop={true}
-        autoPlay={true}
-        showArrows={false}
-        stopOnHover={true}
-        showIndicators={false}
-        onSwipeMove={false}>
+    <>
+      <Helmet>
+        {/* Adaugarea linkului carte ca si asa sa precarce imaginea */}
+        <link rel="preload" as="image" href={poza1} />
+        <link rel="preload" as="image" href={poza2} />
+        <link rel="preload" as="image" href={poza3} />
+      </Helmet>
 
-        {poze.map((p, key) => {
-          return (
-            <div className="inaltime" key={key}>
-              <img src={p} alt="Detailing Suceava"/>
-              
-            </div>
-          );
-        })}
-      </Carousel>
-    </div>
+      <div className="filter height">
+        <Carousel
+          showThumbs={false}
+          showStatus={false}
+          infiniteLoop={true}
+          autoPlay={true}
+          showArrows={false}
+          stopOnHover={true}
+          showIndicators={false}
+          onSwipeMove={false}>
+          
+          {poze.map((p, key) => {
+            return (
+              <div className="inaltime" key={key}>
+                <img src={p} alt="Detailing Suceava"/>
+                
+              </div>
+            );
+          })}
+        </Carousel>
+      </div>
+    </>
   );
 };
 
-export default Carousele;
+export default Carousele
